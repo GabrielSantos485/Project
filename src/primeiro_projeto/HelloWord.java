b3fd254299738972dfe9757d1192e7d3d5e6f446
27GabrielSantos03 <gabrielsan485@gmail.com>
2023-03-26 22:04:51 -0400

    Enviando Primeiro Teste

---
+++ src/primeiro_projeto/HelloWord.java
@@ -0,0 +1,14 @@
+package primeiro_projeto;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int numero1=3;
+		int numero2=5;
+		int resultado= numero1*numero2;
+		System.out.println(resultado);
+	}
+
+}
